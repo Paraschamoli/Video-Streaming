728fb43f87b5a05340c28eef054e3aff18e14bb6
paras chamoli <paraschamoli2592002@gmail.com>
2024-07-11 19:47:32 +0530

    util code

--- src/app.js
+++ src/app.js
@@ -4,39 +4,24 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
+//this is used for cross origon policy
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
+// this is used to accept json file
 app.use(express.json({limit: "16kb"}))
+
+//this is used to accept URL data.
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
+
+//this is used to store file/folder in server
 app.use(express.static("public"))
+
+//this is used perform CRUD operation in cookies
+//access them.....
 app.use(cookieParser())
 
-
-//routes import
-import userRouter from './routes/user.routes.js'
-import healthcheckRouter from "./routes/healthcheck.routes.js"
-import tweetRouter from "./routes/tweet.routes.js"
-import subscriptionRouter from "./routes/subscription.routes.js"
-import videoRouter from "./routes/video.routes.js"
-import commentRouter from "./routes/comment.routes.js"
-import likeRouter from "./routes/like.routes.js"
-import playlistRouter from "./routes/playlist.routes.js"
-import dashboardRouter from "./routes/dashboard.routes.js"
-
-//routes declaration
-app.use("/api/v1/healthcheck", healthcheckRouter)
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/tweets", tweetRouter)
-app.use("/api/v1/subscriptions", subscriptionRouter)
-app.use("/api/v1/videos", videoRouter)
-app.use("/api/v1/comments", commentRouter)
-app.use("/api/v1/likes", likeRouter)
-app.use("/api/v1/playlist", playlistRouter)
-app.use("/api/v1/dashboard", dashboardRouter)
-
-// http://localhost:8000/api/v1/users/register
 
 export { app }
