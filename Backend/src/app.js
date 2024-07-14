88846298114293b5789fb6e562399750ee355502
paras chamoli <paraschamoli2592002@gmail.com>
2024-07-14 16:04:48 +0530

    all setup config

---
+++ Backend/src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+//this is used for cross origon policy
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// this is used to accept json file
+app.use(express.json({ limit: "16kb" }));
+
+//this is used to accept URL data.
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+//this is used to store file/folder in server
+app.use(express.static("public"));
+
+//this is used perform CRUD operation in cookies
+//access them.....
+app.use(cookieParser());
+
+// import routes
+import userRouter from "./routes/user.routes.js"
+
+//routes decleration
+app.use("/api/v1/users",userRouter)
+export { app };
--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -66,7 +66,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 };
 
 userSchema.methods.generateAccessToken = function () {
- return  jwt.sign(
+  return jwt.sign(
     {
       _id: this._id,
       email: this.email,
@@ -81,15 +81,14 @@ userSchema.methods.generateAccessToken = function () {
 };
 
 userSchema.methods.generateRefreshToken = async function () {
-    return  jwt.sign(
-        {
-          _id: this._id,
-
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {
-          expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
-        }
-      );
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
 };
 export const User = mongoose.model("User", userSchema);
